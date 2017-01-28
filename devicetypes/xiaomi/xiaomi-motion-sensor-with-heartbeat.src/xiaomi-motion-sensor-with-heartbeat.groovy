5663bb367e9715e54ef8fc68fd5a4a2578c66294
deusrex2k <deusrex2k@yahoo.com>
2017-01-28 18:20:34 -0500

    Update xiaomi-motion-sensor-with-heartbeat.groovy

--- devicetypes/xiaomi/xiaomi-motion-sensor-with-heartbeat.src/xiaomi-motion-sensor-with-heartbeat.groovy
+++ devicetypes/xiaomi/xiaomi-motion-sensor-with-heartbeat.src/xiaomi-motion-sensor-with-heartbeat.groovy
@@ -22,7 +22,7 @@
  */
 
 metadata {
-	definition (name: "Xiaomi Motion Sensor", namespace: "a4refillpad", author: "a4refillpad") {
+	definition (name: "Xiaomi Motion Sensor", namespace: "xiaomi", author: "a4refillpad") {
 		capability "Motion Sensor"
 		capability "Configuration"
 		capability "Battery"
